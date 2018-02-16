a0b0400c795cfaa2f3150c72653588cd5ce33b99
Geoff Davis <gdavis@archermalmo.com>
2018-02-16 11:21:57 -0600

    update trim function to replace all space-padding at end of string, not just single space

--- src/functions/typeography.ts
+++ src/functions/typeography.ts
@@ -1,19 +1,18 @@
-const trim = (string: string): string => string.replace(/^\s+|\s$/g, "");
-
-const ucFirst = ([firstLetter, ...restLetters]: string): string =>
-  `${firstLetter.toUpperCase()}${restLetters.join("")}`;
-
 const capitalize = (string: string): string =>
   string
     .split(" ")
     .map(s => ucFirst(s))
     .join(" ");
 
-const slugify = (textToSlug: string, delimiter: string = "-"): string => {
-  return textToSlug
+const slugify = (textToSlug: string, delimiter: string = "-"): string =>
+  textToSlug
     .replace(/(\!|#|\$|%|\*|\.|\/|\\|\(|\)|\+|\||\,|\:|\'|\")/g, "")
     .replace(/(.)(\s|\_|\-)+(.)/g, `$1${delimiter}$3`)
     .toLowerCase();
-};
+
+const trim = (string: string): string => string.replace(/^\s+|\s+$/g, "");
+
+const ucFirst = ([firstLetter, ...restLetters]: string): string =>
+  `${firstLetter.toUpperCase()}${restLetters.join("")}`;
 
 export { capitalize, slugify, trim, ucFirst };
