cb8f85fe5f3afa47f61486321165f765ab0a0f3a
Geoff Davis <geoffdavis92@gmail.com>
2018-09-06 01:20:29 -0500

    create tests for searchPropPath function, update sPP API to add optional delimiter arg, allow string path args, throw error when a value resulting from a searched path is undefined

--- src/functions/dataManipulation.ts
+++ src/functions/dataManipulation.ts
@@ -3,20 +3,32 @@
  */
 
 type alphanumeric = string | number;
+type arrayLike = string | alphanumeric[];
 
 /**
  * @function searchPropPath
- * @description Recursively searchs through a data object
+ * @description Recursively searchs through a data object; throws an error if the resulting value of a searched path is undefined.
  * @param {alphanumeric[]} [path] Array of keys in the order of which will be used to recursively search an object
  * @param {object} [collection] Data object
+ * @param {string} [delimiter] Delimiter by which to split the path; defaults to '.'
  * @return {any} Value at the end of the searched property path;
  */
-const searchPropPath = (path: alphanumeric[], collection: object): any => {
+const searchPropPath = (
+  path: arrayLike,
+  collection: object,
+  delimiter: string = "."
+): any => {
+  const safePath = typeof path === "string" ? path.split(delimiter) : path;
   let pathResult = collection;
-  path.forEach(key => {
+  safePath.forEach(key => {
     pathResult = pathResult[key];
   });
-  return pathResult ? pathResult : false;
+  if (pathResult) return pathResult;
+  throw new Error(
+    `pathResult yields undefined value when searching ${safePath.join(
+      delimiter
+    )} on collection argument.`
+  );
 };
 
 export { searchPropPath };
